8825c4a5cf0cf4f42e4af2b4a78aa984aff64ca9
Joe Maddalone <joe.maddalone@gmail.com>
2026-01-07 16:02:32 -0600

    chore: cleaning up unused

--- src/lib/client/database/planned_sets.ts
+++
@@ -1,7 +0,0 @@
-import { plannedSets } from "@/db/schema";
-import { db } from "@/lib/client/database/database";
-import { eq } from "drizzle-orm";
-
-export async function deletePlannedSetsBySessionId(sessionId: number): Promise<void> {
-	await db.delete(plannedSets).where(eq(plannedSets.sessionId, sessionId));
-}
