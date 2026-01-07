f450c5a83abe830cb551a1f66a6009b84fe7bec6
Joe Maddalone <joe.maddalone@gmail.com>
2026-01-07 15:59:55 -0600

    feat(config): initial exercise config form (#10)

--- src/app/(pages)/settings/_components/ExerciseForm.tsx
+++ src/app/(pages)/settings/_components/ExerciseForm.tsx
@@ -4,22 +4,28 @@ import { Card, CardHeader } from "@/components/ui/Card";
 import { Button } from "@/components/ui/Button";
 import { api } from "@/lib/api";
 import { useRouter } from "next/navigation";
-import { useActionState } from "react";
+import { useActionState, useEffect } from "react";
 import { Route } from "next";
 import { ApiResponse } from "@/lib/types";
+import { Barbell } from "@/db/schema";
+import { useState } from "react";
 
 type FormState = {
   exercise_name?: string;
   exercise_category?: string;
+  equipment_type?: string;
 };
 
 const initialState: FormState = {
   exercise_name: "",
   exercise_category: "",
+  equipment_type: "",
 };
 
-export const ExerciseForm = ({ item }: { item?: Exercise }) => {
+export const ExerciseForm = ({ item }: { item?: Exercise; }) => {
   const router = useRouter();
+  const [barbells, setBarbells] = useState<Barbell[]>([]);
+  const [equipmentType, setEquipmentType] = useState(item?.equipmentType || "barbell");
 
   const deleteExercise = async () => {
     if (item?.id) {
@@ -33,11 +39,15 @@ export const ExerciseForm = ({ item }: { item?: Exercise }) => {
   const action = async (state: FormState, formData: FormData) => {
     const name = formData.get("exercise_name")?.toString();
     const category = formData.get("exercise_category")?.toString();
+    const equipmentType = formData.get("equipment_type")?.toString();
+    const barbellId = formData.get("barbell_id")?.toString();
     let response: ApiResponse<Exercise>;
 
-		const newExercise: Partial<Exercise> = {
+    const newExercise: Partial<Exercise> = {
       name: name,
-			category:category
+      category: category,
+      equipmentType: equipmentType,
+      barbellId: Number(barbellId) || undefined
     };
     const isNew = item?.id;
 
@@ -58,6 +68,18 @@ export const ExerciseForm = ({ item }: { item?: Exercise }) => {
     return state;
   };
 
+
+  useEffect(() => {
+    // fetch barbells
+    const doit = async () => {
+      const { data } = await api.barbells.list();
+      if (data) {
+        setBarbells(data);
+      }
+    };
+    doit();
+  }, []);
+
   const [plateData, formAction, pending] = useActionState(action, initialState);
 
   return (
@@ -66,21 +88,53 @@ export const ExerciseForm = ({ item }: { item?: Exercise }) => {
         <form action={formAction}>
           <div className="space-y-2">
             <div className="flex gap-2">
-              <input
-                name="exercise_name"
-                type="text"
-                className="input"
-                defaultValue={item?.name}
-                placeholder="Name"
-              />
-              <input
-                name="exercise_category"
-                type="text"
-                className="input"
-                defaultValue={item?.category}
-                placeholder="Category"
-              />
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend">Name</legend>
+                <input
+                  name="exercise_name"
+                  type="text"
+                  className="input"
+                  defaultValue={item?.name}
+                  placeholder="Bench Press Close-Grip"
+                />
+              </fieldset>
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend">Category</legend>
+                <input
+                  name="exercise_category"
+                  type="text"
+                  className="input"
+                  defaultValue={item?.category}
+                  placeholder="Push, Pull, Legs, etc."
+                />
+              </fieldset>
             </div>
+
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Equipment Type</legend>
+              <select onChange={(e) => setEquipmentType(e.target.value)} defaultValue={item?.equipmentType} name="equipment_type" className="select w-full">
+                <option disabled={true}>Equipment Type</option>
+                <option value="barbell">Barbell</option>
+                <option value="bodyweight">Bodyweight</option>
+                <option value="cable">Cable</option>
+                <option value="dumbbell">Dumbbell</option>
+                <option value="machine">Machine</option>
+              </select>
+            </fieldset>
+            {equipmentType === "barbell" && (
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend">Barbell</legend>
+                <select defaultValue={Number(item?.barbellId) || 1} name="barbell_id" className="select w-full">
+                  <option disabled={true}>Barbell</option>
+                  {barbells.map((barbell) => (
+                    <option key={barbell.id} value={barbell.id}>
+                      {barbell.name}
+                    </option>
+                  ))}
+                </select>
+              </fieldset>
+            )}
+
             <div className="flex justify-between gap-2">
               <div className="space-x-2">
                 <Button
@@ -104,19 +158,19 @@ export const ExerciseForm = ({ item }: { item?: Exercise }) => {
                 </Button>
               </div>
 
-              {item?.id  ? (
-								<div>
-                <Button
-                  type="button"
-                  size="xs"
-                  variant="danger"
-                  disabled={pending}
-                  onClick={deleteExercise}
-                >
-                  Delete
-                </Button>
-              </div>
-							) : null}
+              {item?.id ? (
+                <div>
+                  <Button
+                    type="button"
+                    size="xs"
+                    variant="danger"
+                    disabled={pending}
+                    onClick={deleteExercise}
+                  >
+                    Delete
+                  </Button>
+                </div>
+              ) : null}
             </div>
           </div>
         </form>
