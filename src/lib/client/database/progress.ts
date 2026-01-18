12cf0c2b5c091c3079aae06a10e721561359d8dd
Joe Maddalone <joe.maddalone@gmail.com>
2026-01-18 14:30:13 -0600

    refactor: simplify date calc for progress

--- src/lib/client/database/progress.ts
+++ src/lib/client/database/progress.ts
@@ -3,35 +3,17 @@ import { actualSets, plannedSets, sessions } from "@/db/schema";
 import { eq, gte, sql, and, isNotNull, asc } from "drizzle-orm";
 
 
-export const progressByWeight = async (range: number, exerciseId?: number) => {
-	// range
-	//30 = last 30 days
-	// 90 = last 90 days
-	// 180 = last 180 days
-	// 365 = last 365 days
-	// 0 = all time
-	const startDate = new Date();
-	switch (range) {
-		case 0:
-			startDate.setDate(startDate.getDate() - 9999);
-			break;
-		case 30:
-			startDate.setDate(startDate.getDate() - 30);
-			break;
-		case 90:
-			startDate.setDate(startDate.getDate() - 90);
-			break;
-		case 180:
-			startDate.setDate(startDate.getDate() - 180);
-			break;
-		case 365:
-			startDate.setDate(startDate.getDate() - 365);
-			break;
-		default:
-			startDate.setDate(startDate.getDate() - 30);
-			break;
-	}
+type DiffRange = 0 | 30 | 90 | 180 | 365;
 
+const getStartDate = (int: DiffRange) => {
+	const date = new Date();
+	const dateDiff = int === 0 ? 9999 : int;
+	date.setDate(date.getDate() - dateDiff);
+	return date;
+};
+
+export const progressByWeight = async (range: DiffRange, exerciseId?: number) => {
+	const startDate = getStartDate(range);
 	const result = await db.select({
 		sessionId: sessions.id,
 		date: sessions.date,
