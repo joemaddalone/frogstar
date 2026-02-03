adb33bed1d4ed0ce64b28ed2b7af6c8d293c7d63
Joe Maddalone <joe.maddalone@gmail.com>
2026-02-03 10:43:09 -0600

    feat: common settings page code

--- src/app/(pages)/settings/data/page.tsx
+++
@@ -1,9 +0,0 @@
-export const dynamic = "force-dynamic";
-import { Data } from "@/app/(pages)/settings/_components/Data";
-
-export default async function DataPage() {
-
-	return (
-		<Data />
-	);
-}
