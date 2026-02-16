1e78c847a3e33572a7efbe94d26e9ff682a67ff9
Joe Maddalone <joe.maddalone@gmail.com>
2026-02-16 10:34:40 -0600

    test: run test.db setup as a part of vitest setup

--- vitest.globalSetup.ts
+++ vitest.globalSetup.ts
@@ -1,8 +1,10 @@
 import { reset } from "@/db/reset";
 import { main } from "@/db/seed";
-
+import { execSync } from "child_process";
 
 export async function setup() {
+
+	execSync("npm run drizzle:push");
 	await reset();
 	await main();
 
