b2595043318ff85debab30c3b9eddd5f79051e9e
Joe Maddalone <joe.maddalone@gmail.com>
2026-02-26 09:58:51 -0600

    feat: session header dropdown

--- src/app/(pages)/session/_components/SessionHeader.tsx
+++ src/app/(pages)/session/_components/SessionHeader.tsx
@@ -1,7 +1,7 @@
 "use client";
 import type { Session } from "@/lib/types";
 import { Button } from "@/components/ui/Button";
-import { Copy, Trash } from "lucide-react";
+import { Copy, Trash, Settings } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { api } from "@/lib/api";
 import { useTranslations } from "next-intl";
@@ -25,9 +25,16 @@ export const SessionHeader = ({ session }: { session: Session; }) => {
 	};
 
 	return (
-		<div className="flex items-center space-x-2">
-			<Button onClick={copySession} size="xs" variant="outline">Copy <Copy className="h-4 w-4" /></Button>
-			<Button onClick={deleteSession} size="xs" variant="danger">Delete <Trash className="h-4 w-4" /></Button>
-		</div>
+
+		<div className="dropdown dropdown-bottom dropdown-end">
+			<Button size="icon" variant="outline" tabIndex={0} role="button">
+				<Settings  className="h-4 w-4" />
+			</Button>
+			<ul tabIndex={-1} className="dropdown-content menu bg-base-100 rounded-box z-999 w-52 p-2 shadow-lg">
+				<li className="mb-2"><Button onClick={copySession} size="xs" variant="outline"><Copy className="h-4 w-4" />Copy</Button></li>
+				<li><Button onClick={deleteSession} size="xs" variant="danger"><Trash className="h-4 w-4" />Delete</Button></li>
+			</ul>
+		</div >
+
 	);
 };
